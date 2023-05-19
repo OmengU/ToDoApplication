c10754ade06c5750285d14cd6bc65aed0d3f408f
OmengU <58337160+OmengU@users.noreply.github.com>
2023-05-19 13:57:17 +0200

    small change to backend

--- backend/ToDoAPI/Models/ToDoDto.cs
+++ backend/ToDoAPI/Models/ToDoDto.cs
@@ -1,8 +1,13 @@
-﻿namespace ToDoAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models
 {
     public class ToDoDto
     {
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; }
+        
+        [Required(ErrorMessage = "Content is required")]
         public string Content { get; set; }
     }
 }
